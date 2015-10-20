3b111f880f227a6efe849b02bb9b1f06fc3d59ed
itjhDev <github@itjh.com.cn>
2015-10-21 00:03:26 +0800

    ApiException 类修改

--- src/main/java/me/doushi/api/util/ApiException.java
+++ src/main/java/me/doushi/api/util/ApiException.java
@@ -1,12 +1,62 @@
 package me.doushi.api.util;
 
 /**
+ * {
+ "error_code": 10107,
+ "error": "系统错误",
+ "request": "",
+ "error_detail": "参数（count）值非法，希望得到（int[1~4294967295]），实际得到（0）"
+ }
  * ApiException
  */
 public class ApiException extends Exception{
-	private int code;
-	public ApiException (int code, String msg) {
-		super(msg);
-		this.code = code;
+
+	private int error_code;
+	private String error; //错误标题
+	private String request; //请求路径
+	private String error_detail; //错误描述
+
+	public ApiException() {
+		super();
+	}
+
+	public ApiException(String message, int error_code, String error, String request, String error_detail) {
+		super(message);
+		this.error_code = error_code;
+		this.error = error;
+		this.request = request;
+		this.error_detail = error_detail;
+	}
+
+	public int getError_code() {
+		return error_code;
+	}
+
+	public void setError_code(int error_code) {
+		this.error_code = error_code;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+
+	public String getError_detail() {
+		return error_detail;
+	}
+
+	public void setError_detail(String error_detail) {
+		this.error_detail = error_detail;
 	}
 }
